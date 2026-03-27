3935eed05a476cc9857655100729f23f8a54daf1
ertugrul.karamanli@gmail.com <ertugrul.karamanli@gmail.com>
2026-03-27 17:34:07 +0000

    Added 4 new raw tables and edited schema accordingly.

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,28 @@
+with adwords as (
+    select * from {{ ref('stg_raw__adwords') }}
+),
+
+bing as (
+    select * from {{ ref('stg_raw__bing') }}
+),
+
+criteo as (
+    select * from {{ ref('stg_raw__criteo') }}
+),
+
+facebook as (
+    select * from {{ ref('stg_raw__facebook') }}
+),
+
+-- Tüm kanalları alt alta birleştiriyoruz
+unioned as (
+    select * from adwords
+    union all
+    select * from bing
+    union all
+    select * from criteo
+    union all
+    select * from facebook
+)
+
+select * from unioned
