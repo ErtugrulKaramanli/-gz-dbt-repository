7452a1115e8bd35905ea8b1bca85cbc06cc7a94e
ertugrul.karamanli@gmail.com <ertugrul.karamanli@gmail.com>
2026-03-27 16:29:32 +0000

    Added finance_days mart model

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -9,6 +9,13 @@ shipping as (
 select
     o.orders_id,
     o.date_date,
+    o.revenue,
+    o.quantity,
+    o.purchase_cost,
+    o.margin,
+    s.shipping_fee,
+    s.logcost,
+    s.ship_cost,
     ROUND(o.margin + s.shipping_fee - s.logcost - s.ship_cost,2) as operational_margin
 from orders_operational as o
 left join shipping as s on o.orders_id = s.orders_id
