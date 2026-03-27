b9c909f56bc3eb7bb193665bd936eace11bdf6fe
ertugrul.karamanli@gmail.com <ertugrul.karamanli@gmail.com>
2026-03-27 18:10:18 +0000

    Fixed schema error

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -22,13 +22,17 @@ joined as (
         coalesce(c.ads_cost, 0) as ads_cost,
         coalesce(c.ads_impression, 0) as ads_impression,
         coalesce(c.ads_click, 0) as ads_click,
-        f.quantity,
+        -- finance_days tablandaki gerçek isimleri eşleştiriyoruz:
+        f.total_quantity_sold as quantity,
         f.revenue,
-        f.purchase_cost,
-        f.margin,
-        f.shipping_fee,
-        f.logCost as log_cost,
-        f.ship_cost
+        f.total_purchase_cost as purchase_cost,
+        -- 'margin' finance_days içinde doğrudan yoksa operational_margin kullanılabilir 
+        -- veya int_orders_operational'dan çekilebilir. Şimdilik operational_margin veriyorum:
+        f.operational_margin as margin, 
+        f.total_shipping_fees as shipping_fee,
+        f.total_logistics_costs as log_cost,
+        -- ship_cost genellikle shipping_fee + log_cost toplamıdır:
+        round(f.total_shipping_fees + f.total_logistics_costs, 2) as ship_cost
     from finance_days f
     left join campaigns_day c 
         on f.date_date = c.date_date
